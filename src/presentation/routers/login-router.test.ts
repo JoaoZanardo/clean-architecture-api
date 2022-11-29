5db7132e20ea359a3fd293151f0bb7d051bb9e07
João Zanardo <joaozzz0105@gmail.com>
2022-11-29 00:16:50 -0300

    ensure LoginRouter calls AuthUseCase with correct params

--- src/presentation/routers/login-router.test.ts
+++ src/presentation/routers/login-router.test.ts
@@ -1,13 +1,28 @@
 import { MissingParamError } from "../helpers/missing-param";
 import { LoginRouter } from "./login-router";
 
-const makeSut = (): LoginRouter => {
-    return new LoginRouter();
+const makeSut = () => {
+    class AuthUseCaseSpy {
+        public email: string = '';
+        public password: string = '';
+
+        auth(email: string, password: string) {
+            this.email = email;
+            this.password = password;
+        }
+    }
+
+    const authUseCaseSpy = new AuthUseCaseSpy();
+    const sut = new LoginRouter(authUseCaseSpy);
+    return {
+        sut,
+        authUseCaseSpy
+    }
 };
 
 describe('Login Router', () => {
     it('Should return 400 if no email is provided', async () => {
-        const sut = makeSut();
+        const { sut } = makeSut();
         const htppRequest = {
             body: {
                 password: 'any_password'
@@ -19,7 +34,7 @@ describe('Login Router', () => {
     });
 
     it('Should return 400 if no password is provided', async () => {
-        const sut = makeSut();
+        const { sut } = makeSut();
         const htppRequest = {
             body: {
                 email: 'any_email'
@@ -28,5 +43,18 @@ describe('Login Router', () => {
         const htppResponse = sut.route(htppRequest);
         expect(htppResponse.statusCode).toBe(400);
         expect(htppResponse.body).toEqual(new MissingParamError('password'));
+    });
+
+    it('Should call AuthUseCase with correct params', async () => {
+        const { sut, authUseCaseSpy } = makeSut();
+        const htppRequest = {
+            body: {
+                email: 'any_email',
+                password: 'any_password'
+            }
+        };
+        sut.route(htppRequest);
+        expect(authUseCaseSpy.email).toEqual(htppRequest.body.email);
+        expect(authUseCaseSpy.password).toEqual(htppRequest.body.password);
     });
 });
--- src/presentation/routers/login-router.ts
+++ src/presentation/routers/login-router.ts
@@ -7,10 +7,13 @@ interface Body {
 }
 
 export class LoginRouter {
+    constructor(private authUseCase: any) { }
+
     route(httpRequest: { body: Body }): { statusCode: number, body: MissingParamError } {
         const { email, password } = httpRequest.body;
         if (!email) return HttpResponse.badRequest('email');
         if (!password) return HttpResponse.badRequest('password');
+        this.authUseCase.auth(email, password);
         return { statusCode: 200, body: new MissingParamError('nothing') };
     }
 }
