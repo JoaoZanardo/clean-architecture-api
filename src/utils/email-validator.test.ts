3c50e82ae0f579e4f072e7d93b29d185e699a87e
João Zanardo <joaozzz0105@gmail.com>
2022-12-02 11:12:34 -0300

    ensure EmailValidator calls validator with correct email

--- src/utils/email-validator.test.ts
+++ src/utils/email-validator.test.ts
@@ -2,8 +2,10 @@ import validator from "validator";
 
 class EmailValidator {
     public isEmailValid = true;
+    public email: string = 'null';
 
     isValid(email: string) {
+        this.email = email;
         return validator.isEmail(email);
     }
 }
@@ -15,13 +17,19 @@ const makeSut = () => {
 describe('Email Validator', () => {
     it('Should returns true if validator returns true', () => {
         const sut = makeSut();
-        const isEmailValid = sut.isValid('valid_email@gmail.com')
+        const isEmailValid = sut.isValid('valid_email@gmail.com');
         expect(isEmailValid).toBeTruthy();
     });
     it('Should returns false if validator returns false', () => {
         const sut = makeSut();
         sut.isEmailValid = false;
-        const isEmailValid = sut.isValid('invalid_email')
+        const isEmailValid = sut.isValid('invalid_email');
         expect(isEmailValid).toBeFalsy();
+    });
+
+    it('Should call validator with correct email', () => {
+        const sut = makeSut();
+        sut.isValid('any_email@gmail.com');
+        expect(sut.email).toEqual('any_email@gmail.com');
     });
 });
