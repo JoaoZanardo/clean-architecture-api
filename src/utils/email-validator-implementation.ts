f6f367f544bbf42f7fce1779aa9b4549b1d09939
João Zanardo <joaozzz0105@gmail.com>
2022-12-11 20:40:44 -0300

    type EmailValidatorImplementation returns

--- src/utils/email-validator-implementation.ts
+++ src/utils/email-validator-implementation.ts
@@ -2,7 +2,7 @@ import { EmailValidator } from '../interfaces/email-validator';
 import validator from 'validator';
 
 export class EmailValidatorImplementation implements EmailValidator {
-    isValid(email: string) {
+    isValid(email: string): boolean {
         return validator.isEmail(email);
     }
 }
