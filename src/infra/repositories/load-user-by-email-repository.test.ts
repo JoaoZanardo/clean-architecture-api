e771d6c625cff51ddbb6eaac1b041d650945df0a
João Zanardo <joaozzz0105@gmail.com>
2022-12-22 15:45:22 -0300

    improve LoadUserByEmailRepository´s tests

--- src/infra/repositories/load-user-by-email-repository.test.ts
+++ src/infra/repositories/load-user-by-email-repository.test.ts
@@ -1,17 +1,29 @@
 import { Collection, Document, MongoClient, WithId } from 'mongodb';
 
 interface User {
-    email: string
+    name: string;
+    email: string;
+    password: string;
+    city: string;
+    country: string;
 }
 
 class LoadUserByEmailRepository {
     constructor(private userModel: Collection<Document>) { }
 
     async load(email: string): Promise<WithId<Document> | null> {
-        const user = await this.userModel.findOne({ email });
+        const user = await this.userModel.findOne({ email }, {
+            projection: {
+                password: 1
+            }
+        });
         return user;
     }
 }
+
+const makeSut = (userModel: Collection<Document>) => {
+    return new LoadUserByEmailRepository(userModel);
+};
 
 describe('LoadUserByEmail Repository', () => {
     let userModel: Collection<Document>;
@@ -32,17 +44,21 @@ describe('LoadUserByEmail Repository', () => {
     });
 
     it('Should returns null if no user is found', async () => {
-        const sut = new LoadUserByEmailRepository(userModel);
+        const sut = makeSut(userModel);
         const user = await sut.load('invalid_email@email.com');
         expect(user).toBeNull();
     });
 
     it('Should returns an user if user is found', async () => {
-        const sut = new LoadUserByEmailRepository(userModel);
-        await userModel.insertOne({
-            email: 'valid_email@email.com'
+        const sut = makeSut(userModel);
+        const fakeUser = await userModel.insertOne({
+            name: 'any_name',
+            city: 'any_city',
+            country: 'any_country',
+            email: 'valid_email@email.com',
+            password: 'hashed_password'
         });
         const user = await sut.load('valid_email@email.com');
-        expect(user?.email).toEqual('valid_email@email.com');
+        expect(user?._id).toEqual(fakeUser.insertedId);
     });
 });
