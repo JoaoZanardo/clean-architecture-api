73b9da9f51e0ee08676e0dc94d822cb5c61c5ad3
João Zanardo <joaozzz0105@gmail.com>
2023-01-04 16:03:17 -0300

    resolve DB connect issues

--- src/infra/repositories/load-user-by-email-repository.ts
+++ src/infra/repositories/load-user-by-email-repository.ts
@@ -1,10 +1,11 @@
 import { Document, WithId } from "mongodb";
+import env from "../../main/config/env";
 import { LoadUserByEmailRepository } from "../../interfaces/load-user-by-email-repository";
 import mongoHelper from "../helpers/mongo-helper";
 
 export class DbLoadUserByEmailRepository implements LoadUserByEmailRepository<WithId<Document>> {
     async load(email: string): Promise<WithId<Document> | null> {
-        await mongoHelper.connect(process.env.MONGO_URL as string);
+        await mongoHelper.connect(env.mongoUrl);
         const userModel = await mongoHelper.getCollection('users');
         const user = await userModel.findOne({ email }, {
             projection: {
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,4 @@
 export default {
-    mongoUrl: process.env.MONGO_URL || 'mongodb+srv://joaoZanardo:pinguim2005ZZZ@cursojs01.hsoem.mongodb.net/?retryWrites=true&w=majority',
+    mongoUrl: process.env.MONGO_URL || 'mongodb+srv://joaoZanardo:pinguim2005ZZZ@cursojs01.hsoem.mongodb.net/ca-api?retryWrites=true&w=majority',
     tokenScret: '11011'
 }
