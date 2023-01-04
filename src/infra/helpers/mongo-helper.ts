bfd6b7ea0cb3c477995dc8f2d3042142d7adec0e
João Zanardo <joaozzz0105@gmail.com>
2023-01-03 23:50:52 -0300

    add getColletion method

--- src/infra/helpers/mongo-helper.ts
+++ src/infra/helpers/mongo-helper.ts
@@ -2,15 +2,16 @@ import { Collection, Db, Document, MongoClient } from 'mongodb';
 
 class MongoHelper {
     private client: MongoClient | null = null;
+    private db: Db | null = null;
 
     async connect(uri: string, dbName: string): Promise<{ client: MongoClient, db: Db }> {
         this.client = await MongoClient.connect(uri, {});
         return { client: this.client, db: this.client.db(dbName) };
     }
 
-    async getDb(): Promise<Db | null> {
-        if (!this.client) return null;
-        return this.client.db();
+    async getColletion(name: string): Promise<Collection<Document> | null> {
+        if (!this.client || !this.db) return null;
+        return this.db.collection(name);
     }
 
     async disconnect(): Promise<void> {
