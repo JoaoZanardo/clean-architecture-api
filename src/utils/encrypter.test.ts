7694d000607232ef01f353d0f4c07bfa865b60db
João Zanardo <joaozzz0105@gmail.com>
2023-01-05 20:40:10 -0300

    improve encrypter tests

--- src/utils/encrypter.test.ts
+++ src/utils/encrypter.test.ts
@@ -1,23 +1,50 @@
-import { Bcrypt } from "./bcrypt";
+import bcrypt from 'bcrypt';
+import { Bcrypt } from './bcrypt';
 
-jest.mock("./bcrypt");
+interface MockedBcrypt {
+    isValid: boolean;
+    value: string;
+    hash: string;
+}
+
+jest.mock('bcrypt', () => ({
+    value: '',
+    hash: '',
+    isValid: true,
+
+    async compare(value: string, hash: string) {
+        this.value = value
+        this.hash = hash
+        return this.isValid
+    },
+}));
 
 const makeSut = () => {
-    return new Bcrypt() as jest.Mocked<Bcrypt>;
+    const mockedBcrypt = bcrypt as unknown as jest.Mocked<MockedBcrypt>;
+    return {
+        sut: new Bcrypt(),
+        mockedBcrypt
+    }
 };
 
 describe('Encrypter', () => {
     it('Should returns true if encrypter returns true', async () => {
-        const sut = makeSut();
-        sut.compare.mockResolvedValueOnce(true);
+        const { sut } = makeSut();
         const isValid = await sut.compare('any_value', 'hashed_value');
         expect(isValid).toBeTruthy();
     });
 
     it('Should returns false if encrypter returns false', async () => {
-        const sut = makeSut();
-        sut.compare.mockResolvedValueOnce(false);
+        const { sut, mockedBcrypt } = makeSut();
+        mockedBcrypt.isValid = false;
         const isValid = await sut.compare('any_value', 'hashed_value');
         expect(isValid).toBeFalsy();
+    });
+
+    it('Should call bcrypt with correct values', async () => {
+        const { sut, mockedBcrypt } = makeSut();
+        await sut.compare('any_value', 'hashed_value');
+        expect(mockedBcrypt.value).toEqual('any_value');
+        expect(mockedBcrypt.hash).toEqual('hashed_value');
     });
 });
