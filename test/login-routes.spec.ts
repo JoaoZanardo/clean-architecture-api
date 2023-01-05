2c6c58661eae3e0d63e531684ff0bea22c9fd086
João Zanardo <joaozzz0105@gmail.com>
2023-01-04 23:13:45 -0300

    add login route integration test to ensure 200 response when valid credentials are provided

---
+++ test/login-routes.spec.ts
@@ -0,0 +1,35 @@
+import { Collection, Document } from 'mongodb';
+import { agent } from 'supertest';
+import app from '../src/main/config/app';
+import mongoHelper from '../src/infra/helpers/mongo-helper';
+import bcrypt from 'bcrypt';
+import env from '../src/main/config/env';
+
+describe('Login Routes', () => {
+    let userModel: Collection<Document>;
+
+    beforeAll(async () => {
+        await mongoHelper.connect(env.mongoUrl);
+        userModel = await mongoHelper.getCollection('users');
+    });
+
+    beforeEach(async () => {
+        await userModel.deleteMany({});
+    });
+
+    afterAll(async () => {
+        await mongoHelper.disconnect();
+    });
+
+    it('Should return 200 when valid creditials are provided', async () => {
+        await userModel.insertOne({
+            email: 'valid@email.com',
+            password: await bcrypt.hash('valid_password', 10)
+        });
+
+        await agent(app)
+            .post('/api/login')
+            .send({ email: 'valid@email.com', password: 'valid_password' })
+            .expect(200);
+    });
+});
