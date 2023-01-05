e2052648ffdeffe8b85a27524e9268bd71d795fe
João Zanardo <joaozzz0105@gmail.com>
2023-01-05 20:54:29 -0300

    improve email-validator tests

--- src/utils/email-validator.test.ts
+++ src/utils/email-validator.test.ts
@@ -1,23 +1,45 @@
 import { Validator } from './validator';
+import validator from 'validator';
 
-jest.mock('./validator');
+interface MockedValidator {
+    email: string;
+    isEmailValid: boolean;
+}
+
+jest.mock('validator', () => ({
+    email: '',
+    isEmailValid: true,
+
+    isEmail(email: string): boolean {
+        this.email = email;
+        return this.isEmailValid
+    },
+}));
 
 const makeSut = () => {
-    return new Validator() as jest.Mocked<Validator>;
+    return {
+        sut: new Validator(),
+        mockedValidator: validator as unknown as jest.Mocked<MockedValidator>
+    }
 };
 
 describe('Email Validator', () => {
     it('Should returns true if validator returns true', () => {
-        const sut = makeSut();
-        sut.isValid.mockReturnValueOnce(true);
-        const isEmailValid = sut.isValid('valid_email@gmail.com');
+        const { sut } = makeSut();
+        const isEmailValid = sut.isValid('valid_email');
         expect(isEmailValid).toBeTruthy();
     });
 
     it('Should returns false if validator returns false', () => {
-        const sut = makeSut();
-        sut.isValid.mockReturnValueOnce(false);
+        const { sut, mockedValidator } = makeSut();
+        mockedValidator.isEmailValid = false;
         const isEmailValid = sut.isValid('invalid_email');
         expect(isEmailValid).toBeFalsy();
+    });
+
+    it('Should call validator with correct value', () => {
+        const { sut, mockedValidator } = makeSut();
+        sut.isValid('valid_email');
+        expect(mockedValidator.email).toEqual('valid_email');
     });
 });
