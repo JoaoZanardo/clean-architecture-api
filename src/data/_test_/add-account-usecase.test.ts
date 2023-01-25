42bb8d9986d4d96eb2fd4e005ea54af472fcb037
João Zanardo <joaozzz0105@gmail.com>
2023-01-25 11:44:19 -0300

    ensure AddAccountUseCase returns false if email already exists

--- src/data/_test_/add-account-usecase.test.ts
+++ src/data/_test_/add-account-usecase.test.ts
@@ -1,13 +1,34 @@
+import { LoadUserByEmailRepositorySpy } from "./mocks";
+
 class AddAcountUseCaseService {
-    async add(): Promise<boolean> {
+    constructor(private loadUserByEmailRepo: LoadUserByEmailRepositorySpy) { }
+
+    async add(email: string): Promise<boolean> {
+        const exists = await this.loadUserByEmailRepo.load(email);
+        if (exists) return false;
         return true;
     }
 }
 
+const makeSut = () => {
+    const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy();
+    return {
+        loadUserByEmailRepositorySpy,
+        sut: new AddAcountUseCaseService(loadUserByEmailRepositorySpy)
+    }
+}
+
 describe('AddAccount Usecase', () => {
-    it('Should return true if account was created succesfully', async () => {
-        const sut = new AddAcountUseCaseService();
-        const userCreated = await sut.add();
+    it('Should returns true if account was created succesfully', async () => {
+        const { sut, loadUserByEmailRepositorySpy } = makeSut();
+        loadUserByEmailRepositorySpy.user = null;
+        const userCreated = await sut.add('valid_and_new_email');
         expect(userCreated).toBeTruthy();
+    });
+
+    it('Should returns false if email already exists', async () => {
+        const { sut } = makeSut();
+        const userCreated = await sut.add('valid_and-exists_email');
+        expect(userCreated).toBeFalsy()
     });
 });
