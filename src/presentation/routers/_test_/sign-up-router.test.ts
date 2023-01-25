075103c4110180cc7cdbcf6dd11495bc16438194
João Zanardo <joaozzz0105@gmail.com>
2023-01-25 15:41:43 -0300

    ensure SignUpRouter returns 400 if no name is provided

--- src/presentation/routers/_test_/sign-up-router.test.ts
+++ src/presentation/routers/_test_/sign-up-router.test.ts
@@ -4,6 +4,7 @@ import { EmailValidatorSpy } from "./mocks/mock-email-validator";
 
 type Request = {
     body: {
+        name?: string
         email?: string;
     }
 }
@@ -12,7 +13,8 @@ class SignUpRouter {
     constructor(private emailValidator: EmailValidatorSpy) { }
 
     async handle(request: Request): Promise<HttpResponse> {
-        const { email } = request.body
+        const { email, name } = request.body
+        if (!name) return HttpResponse.badRequest(new MissingParamError('name'));
         if (!email) return HttpResponse.badRequest(new MissingParamError('email'));
         if (!this.emailValidator.isValid(email)) {
             return HttpResponse.badRequest(new InvalidParamError('email'));
@@ -31,10 +33,24 @@ const makeSut = () => {
 }
 
 describe('SignUp Router', () => {
+    it('Should returns 400 if no name is provided', async () => {
+        const { sut } = makeSut();
+        const httpRequest = {
+            body: {
+                email: 'valid_email'
+            }
+        }
+        const response = await sut.handle(httpRequest);
+        expect(response.statusCode).toEqual(400);
+        expect(response.body).toEqual(new MissingParamError('name'));
+    });
+
     it('Should returns 400 if no email is provided', async () => {
         const { sut } = makeSut();
         const httpRequest = {
-            body: {}
+            body: {
+                name: 'any_name'
+            }
         }
         const response = await sut.handle(httpRequest);
         expect(response.statusCode).toEqual(400);
@@ -46,6 +62,7 @@ describe('SignUp Router', () => {
         emailValidator.isEmailValid = false;
         const httpRequest = {
             body: {
+                name: 'any_name',
                 email: 'invalid_email'
             }
         }
